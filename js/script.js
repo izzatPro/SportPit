28cbd5ce77a31e55412ac16f9a78053d4dbc016c
Izzat <superizzat564@gmail.com>
2022-11-25 15:17:48 +0500

    Modal window complited

--- js/script.js
+++ js/script.js
@@ -108,4 +108,37 @@ $(document).ready(function(){
 
     setClock('.timer', deadline);
 
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+          modalTrigger.forEach(btn => {
+            btn.addEventListener('click', function(){
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow = 'hidden';
+            });
+          });
+
+          function closeModal(){
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+          }
+          modalCloseBtn.addEventListener('click',closeModal);
+          modal.addEventListener('click', (e) =>{
+            console.log(e.target);
+            if (e.target === modal ) {
+                closeModal();
+            }
+          });
+
+          document.addEventListener('keydown', (e) =>{
+            if (e.code === 'Escape' && modal.classList.contains('show')){
+                closeModal();
+            }
+          });
+
+
 });
